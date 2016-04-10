79ed3e4fca6d5ac6efe2206a5b05d8055e00238f
jennypilay <nashlyascepilay@hotmail.com>
2016-04-10 17:03:48 -0500

    se agrego boton spoiler

--- js/app.js
+++ js/app.js
@@ -3,9 +3,15 @@
 //mostrar el mensaje lentamente usamos atajo
 //$(".advertencia").show("slow");
 
-function enviarAdvertencia()
-{
-jQuery(".advertencia").hide().show("slow")
-}
+//function enviarAdvertencia()
+//{
+//jQuery(".advertencia").hide().show("slow")
+//}
 //le pasamos al handler la funcion sin parentesis  
-$(document).ready(enviarAdvertencia)
+//$(document).ready(enviarAdvertencia)
+
+//!.ESCONDER spOILER
+$(".spoiler span").hide();
+
+//2.agregar boton
+$(".spoiler").append("<button>mostrar spoiler!</button>");
